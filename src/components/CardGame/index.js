f772107e195ddfcd7931277cbfb946bd1010943c
carloskauan <carloskauan190@gmail.com>
2022-04-01 14:52:32 -0300

    Criando pasta .\src\ com os components do game

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,18 @@
+// # Função
+function CardGame() {
+    return `
+        <article> 
+            CardGame - Salve Malandragem   
+        </article>
+    `;
+}
+
+export default CardGame;
+
+/* # arrow Function
+const CardGame = () => {}
+*/
+
+/* # Function sendo atribuida para uma variável 
+const CardGame = function(){}
+*/
