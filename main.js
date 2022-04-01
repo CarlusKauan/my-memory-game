981a1bd88aeb6c1b30e19ec38a6b9b69e11c463e
carloskauan <carloskauan190@gmail.com>
2022-04-01 14:51:30 -0300

    chamando  com insertAdjacent, e chamando component CardGame()

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+import CardGame from './src/components/CardGame';
+
 // Consumindo a API DOM para manipular elementos HTML
 const $root = document.querySelector("#root")
-// chamando a $root
-$root.textContent = 'Carlos Kauan';
+const $htmlCardGame = CardGame();
+
+// chamando a $root, com insertAdjacent para adicionar um elemento !
+$root.insertAdjacentHTML("beforeend", $htmlCardGame)
 
 
 
