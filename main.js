c7da6e2cbb065b85a8d62e23426884aea21d13e5
carloskauan <carloskauan190@gmail.com>
2022-04-02 18:05:52 -0300

    :construction: Create PlayerName component
    git

--- main.js
+++ main.js
@@ -3,16 +3,17 @@ import './src/styles/generic/reset.css';
 import './src/styles/elements/base.css';
 
 import CardGame from './src/components/CardGame';
-import PlayerTitle from './src/components/PlayerTitle';
+import PlayerName from './src/components/PlayerName';
 import BoardGame from './src/objects/BoardGame/';
 
 // Consumindo a API DOM para manipular elementos HTML
 const $root = document.querySelector("#root")
-const $htmlBoardGame = BoardGame(6);
-const $htmlPlayerTitle = PlayerTitle('Player 1') + PlayerTitle('Player 2')
 // chamando a $root, com insertAdjacent para adicionar um elemento !
-$root.insertAdjacentHTML("beforebegin", $htmlPlayerTitle)
-$root.insertAdjacentHTML("beforeend", $htmlBoardGame)
-
-
-
+$root.insertAdjacentHTML(
+    "beforeend",
+    `
+        ${PlayerName('Player 1')}
+        ${PlayerName('Player 2')}
+        ${BoardGame(6)}
+    `
+);
---
+++ src/components/PlayerName/index.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+function PlayerName(content){
+    return `
+    <div class="container">
+        <p class="Player-title">${content}</p>
+    </div>
+    `;
+}
+
+export default PlayerName;
