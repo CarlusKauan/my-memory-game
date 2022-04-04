33319d9a9b718de08809c622921386315f90ffc6
carloskauan <carloskauan190@gmail.com>
2022-04-04 12:20:38 -0300

    :construction: Create Component CardGameFrontBack

---
+++ src/components/CardFrontBack/index.js
@@ -0,0 +1,13 @@
+import './style.css'
+import CardGame from '../CardGame'
+
+function CardFrontBack(){
+    return /*html*/ `
+        <article class="card-front-back">
+            ${CardGame()}
+            ${CardGame()}
+        </article>        
+    `;
+}
+
+export default CardFrontBack
